d6b9e13bc691c26104b2227b2a6083f42a09899c
Pro-Mo <m.a.schoonhoven@gmail.com>
2014-12-20 20:48:46 +0100

    Tested more thoroughly; Added some comments and removed some redundant code

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
-# This script contists of 2 functions to optimize processing time of inversion given matrix,
+# Caching the Inverse of a Matrix
+# This script contists of 2 functions to optimize processing time of inversion of a given matrix,
 # by caching it in local memory.
-# 1st function (makeCacheMatrix) is for loading the matrix and its inverse in local memory (cache)
-# 2nd function (cacheSolve) does the actual work and before calculating, 
-# checks whether the calculation on that matrix is already done to avoid any redudant processing;
+# Note that for this script the matrix supplied is always invertible!
 
+# This 1st function (makeCacheMatrix) is for loading a matrix object, and for caching the inverse
 makeCacheMatrix <- function(x = matrix()) {
-      # set empty value of the matrix
+      # set empty matrix-object
       s <- NULL
       set <- function(y) {    
             x <<- y
@@ -14,21 +14,24 @@ makeCacheMatrix <- function(x = matrix()) {
       # get the value of the matrix
       get <- function() { x }
       # set the value of the solve function (solve = inverse)
-      setsolve <- function(solve) {s <<- solve } 
+      setsolve <- function(solve) {
+            s <<- solve 
+      }
+      # get the value of the solved function
       getsolve <- function() { s }
       # put all information on what is done in a list
       list(set = set, get = get,
            setsolve = setsolve,
            getsolve = getsolve)
 }
-
-cacheSolve <- function(x = matrix(), ...) {
+# 2nd function (cacheSolve) does the actual work and before calculating, 
+# checks whether the calculation on that matrix is already done to avoid any redudant processing;
+cacheSolve <- function(x = matrix()) {
       # get inverse matrix
       s <- x$getsolve()
       # if this inverse matrix is not null, return it and end of function
       if(!is.null(s)) {
-            message("inverse of matrix already processed before")
-            #return(s)
+            return(s)
       }
       # else get inverse via makeCacheMatrix function
       data <- x$get()
